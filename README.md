b2dd795e51bc795ad4fd073abe80cd69283050cd
amygdalama <amyehanlon@gmail.com>
2014-03-11 09:56:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Learning about Python internals by replacing them with Harry Potter spells, because why wouldn't you do such a thing.
+
+Add the line
+
+```
+export PYTHONSTARTUP=~/path/to/harrypotter.py
+```
+to your `~/.bashrc` or `~/.bash_profile` to really screw things up.
---
+++ harrypotter.py
@@ -0,0 +1,60 @@
+import sys
+import random
+import linecache
+import inspect
+
+wingardium_leviosa = __builtins__.float
+del __builtins__.float
+
+reducto = __builtins__.reduce
+del __builtins__.reduce
+
+avada_kedavra = __builtins__.quit
+del __builtins__.quit
+del __builtins__.exit
+
+
+class HogwartsHouse(object):
+    
+    def __init__(self):
+        self.house = random.choice([Gryffindor(), Hufflepuff(), Ravenclaw(), Slytherin()])
+
+class Gryffindor(HogwartsHouse):
+
+    def __init__(self):
+        pass
+
+class Hufflepuff(HogwartsHouse):
+
+    def __init__(self):
+        pass  
+
+class Ravenclaw(HogwartsHouse):
+
+    def __init__(self):
+        pass
+
+class Slytherin(HogwartsHouse):
+
+    def __init__(self):
+        pass
+
+
+def type(thing, cache={}):
+    if thing not in cache:
+        sorting_hat = HogwartsHouse()
+        cache[thing] = sorting_hat.house
+    return cache[thing]
+
+
+__builtins__.type = type
+
+
+# def trace(frame, event, arg):
+#     print frame, event, arg
+#     if event == 'call':
+#         print frame.f_globals.keys()
+#         line_text = linecache.getline(frame.f_globals["__file__"], frame.f_lineno)
+#         print line_text
+
+# sys.settrace(trace)
